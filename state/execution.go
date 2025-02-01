d81ebacdba6b6dc2b79b53381b685978341c89e9
Vaibhav Jindal <vaibhavjindal@Vaibhavs-MacBook-Pro.local>
2025-02-01 15:37:18 +0530

    add: implemented the feedback loop to keep the avg blocktime const

--- state/execution.go
+++ state/execution.go
@@ -234,29 +234,7 @@ func (blockExec *BlockExecutor) applyBlock(state State, blockID types.BlockID, b
 	})
 	endTime := time.Now().UnixNano()
 
-	if block.Height > 1 {
-		// In the following code, nextBlockDelay is being set based on block time
-		// taken from consensus parameters and time spend since the block.Time.
-		consensusParams, err := blockExec.Store().LoadConsensusParams(int64(block.Height - 1))
-		if err != nil {
-			blockExec.logger.Error("error in fetching the consensus params at height %d", int64(block.Height-1), "err", err)
-			return state, err
-		}
-
-		blockTime := consensusParams.Block.BlockTime
-		nextBlockDelay := 10 * time.Millisecond
-
-		currentTime := time.Now().UTC()
-
-		if currentTime.After(block.Time) {
-			difference := blockTime - (currentTime.Sub(block.Time))
-			if difference > 0 {
-				nextBlockDelay = difference
-			}
-		}
-
-		abciResponse.NextBlockDelay = nextBlockDelay
-	}
+	abciResponse.NextBlockDelay = CalculateDelay(blockExec.store, block)
 
 	blockExec.metrics.BlockProcessingTime.Observe(float64(endTime-startTime) / 1000000)
 	if err != nil {
@@ -696,6 +674,7 @@ func updateState(
 		LastResultsHash:                  TxResultsHash(abciResponse.TxResults),
 		AppHash:                          nil,
 		NextBlockDelay:                   abciResponse.NextBlockDelay,
+		GenesisTime:                      state.GenesisTime,
 	}, nil
 }
 
---
+++ state/util_test.go
@@ -0,0 +1,302 @@
+package state_test
+
+import (
+	"errors"
+	"testing"
+	"time"
+
+	"github.com/cometbft/cometbft/state"
+	"github.com/cometbft/cometbft/state/mocks"
+	"github.com/cometbft/cometbft/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestStateError(t *testing.T) {
+	blockStore := &mocks.Store{}
+
+	genesisTime := time.Now()
+	blockTime := 1 * time.Second
+
+	// empty state
+	mockState := state.State{}
+
+	consensusParams := types.ConsensusParams{
+		Block: types.BlockParams{
+			BlockTime: blockTime,
+		},
+	}
+
+	height := int64(10)
+
+	header := types.Header{
+		Height: height,
+		Time:   genesisTime.Add(9 * blockTime),
+	}
+
+	currentBlock := &types.Block{
+		Header: header,
+	}
+
+	blockStore.On("Load").Return(mockState, errors.New("error"))
+	blockStore.On("LoadConsensusParams", (height-1)).Return(consensusParams, nil)
+
+	delay := state.CalculateDelay(blockStore, currentBlock)
+
+	// when there is any error we expect the 0 because it is handled itseld later on
+	expectedDelay := time.Duration(0)
+
+	assert.Equal(t, expectedDelay, delay)
+}
+
+func TestConsensusParamError(t *testing.T) {
+	blockStore := &mocks.Store{}
+
+	genesisTime := time.Now()
+	blockTime := 1 * time.Second
+
+	mockState := state.State{
+		GenesisTime: genesisTime,
+	}
+
+	// empty consensus params
+	consensusParams := types.ConsensusParams{}
+
+	height := int64(10)
+
+	header := types.Header{
+		Height: height,
+		Time:   genesisTime.Add(9 * blockTime),
+	}
+
+	currentBlock := &types.Block{
+		Header: header,
+	}
+
+	blockStore.On("Load").Return(mockState, nil)
+	blockStore.On("LoadConsensusParams", (height-1)).Return(consensusParams, errors.New("error"))
+
+	delay := state.CalculateDelay(blockStore, currentBlock)
+
+	// when there is any error we expect the 0 because it is handled itseld later on
+	expectedDelay := time.Duration(0)
+
+	assert.Equal(t, expectedDelay, delay)
+}
+
+func TestCalculateDelayWhenDiffIsZero(t *testing.T) {
+	blockStore := &mocks.Store{}
+
+	genesisTime := time.Now()
+	blockTime := 1 * time.Second
+
+	mockState := state.State{
+		GenesisTime: genesisTime,
+	}
+
+	consensusParams := types.ConsensusParams{
+		Block: types.BlockParams{
+			BlockTime: blockTime,
+		},
+	}
+
+	height := int64(10)
+
+	header := types.Header{
+		Height: height,
+		Time:   genesisTime.Add(9 * time.Second),
+	}
+
+	currentBlock := &types.Block{
+		Header: header,
+	}
+
+	blockStore.On("Load").Return(mockState, nil)
+	blockStore.On("LoadConsensusParams", (height-1)).Return(consensusParams, nil)
+
+	delay := state.CalculateDelay(blockStore, currentBlock)
+
+	// ideal next block delay (assuming 10% of block time lost in execution and network delays)
+	idealExpectedDelay := blockTime - blockTime/10
+
+	assert.Equal(t, idealExpectedDelay, delay)
+}
+
+func TestCalculateDelayWhenDiffIsMorePositiveThanMaxDeflection(t *testing.T) {
+	blockStore := &mocks.Store{}
+
+	genesisTime := time.Now()
+	blockTime := 1 * time.Second
+
+	mockState := state.State{
+		GenesisTime: genesisTime,
+	}
+
+	consensusParams := types.ConsensusParams{
+		Block: types.BlockParams{
+			BlockTime: blockTime,
+		},
+	}
+
+	// ideal next block delay (assuming 10% of block time lost in execution and network delays)
+	idealExpectedDelay := blockTime - blockTime/10
+
+	// maxdepliction is 20% of idealExpectedDelay
+	maxDeflection := idealExpectedDelay / 5
+
+	height := int64(10)
+	addedDiff := maxDeflection + maxDeflection/2
+
+	header := types.Header{
+		Height: height,
+		Time:   genesisTime.Add(9 * time.Second).Add(addedDiff),
+	}
+
+	currentBlock := &types.Block{
+		Header: header,
+	}
+
+	blockStore.On("Load").Return(mockState, nil)
+	blockStore.On("LoadConsensusParams", (height-1)).Return(consensusParams, nil)
+
+	delay := state.CalculateDelay(blockStore, currentBlock)
+
+	// as addedDiff is more than the maxDeflection we expect the idealExpectedDelay - maxDeflection
+	expectedDelay := idealExpectedDelay - maxDeflection
+
+	assert.Equal(t, expectedDelay, delay)
+}
+
+func TestCalculateDelayWhenDiffIsLessPositiveThanMaxDeflection(t *testing.T) {
+	blockStore := &mocks.Store{}
+
+	genesisTime := time.Now()
+	blockTime := 1 * time.Second
+
+	mockState := state.State{
+		GenesisTime: genesisTime,
+	}
+
+	consensusParams := types.ConsensusParams{
+		Block: types.BlockParams{
+			BlockTime: blockTime,
+		},
+	}
+
+	// ideal next block delay (assuming 10% of block time lost in execution and network delays)
+	idealExpectedDelay := blockTime - blockTime/10
+
+	// maxdepliction is 20% of idealExpectedDelay
+	maxDeflection := idealExpectedDelay / 5
+
+	height := int64(10)
+	addedDiff := maxDeflection - maxDeflection/2
+
+	header := types.Header{
+		Height: height,
+		Time:   genesisTime.Add(9 * time.Second).Add(addedDiff),
+	}
+
+	currentBlock := &types.Block{
+		Header: header,
+	}
+
+	blockStore.On("Load").Return(mockState, nil)
+	blockStore.On("LoadConsensusParams", (height-1)).Return(consensusParams, nil)
+
+	delay := state.CalculateDelay(blockStore, currentBlock)
+
+	// as addedDiff is less than the maxDeflection we expect the idealExpectedDelay - addedDiff
+	expectedDelay := idealExpectedDelay - addedDiff
+
+	assert.Equal(t, expectedDelay, delay)
+}
+
+func TestCalculateDelayWhenDiffIsMoreNegativeThanMaxDeflection(t *testing.T) {
+	blockStore := &mocks.Store{}
+
+	genesisTime := time.Now()
+	blockTime := 1 * time.Second
+
+	mockState := state.State{
+		GenesisTime: genesisTime,
+	}
+
+	consensusParams := types.ConsensusParams{
+		Block: types.BlockParams{
+			BlockTime: blockTime,
+		},
+	}
+
+	// ideal next block delay (assuming 10% of block time lost in execution and network delays)
+	idealExpectedDelay := blockTime - blockTime/10
+
+	// maxdepliction is 20% of idealExpectedDelay
+	maxDeflection := idealExpectedDelay / 5
+
+	height := int64(10)
+	addedDiff := -1 * (maxDeflection + maxDeflection/2)
+
+	header := types.Header{
+		Height: height,
+		Time:   genesisTime.Add(9 * time.Second).Add(addedDiff),
+	}
+
+	currentBlock := &types.Block{
+		Header: header,
+	}
+
+	blockStore.On("Load").Return(mockState, nil)
+	blockStore.On("LoadConsensusParams", (height-1)).Return(consensusParams, nil)
+
+	delay := state.CalculateDelay(blockStore, currentBlock)
+
+	// as addedDiff is more than the maxDeflection we expect the idealExpectedDelay - maxDeflection
+	expectedDelay := idealExpectedDelay + maxDeflection
+
+	assert.Equal(t, expectedDelay, delay)
+}
+
+func TestCalculateDelayWhenDiffIsLessNegativeThanMaxDeflection(t *testing.T) {
+	blockStore := &mocks.Store{}
+
+	genesisTime := time.Now()
+	blockTime := 1 * time.Second
+
+	mockState := state.State{
+		GenesisTime: genesisTime,
+	}
+
+	consensusParams := types.ConsensusParams{
+		Block: types.BlockParams{
+			BlockTime: blockTime,
+		},
+	}
+
+	// ideal next block delay (assuming 10% of block time lost in execution and network delays)
+	idealExpectedDelay := blockTime - blockTime/10
+
+	// maxdepliction is 20% of idealExpectedDelay
+	maxDeflection := idealExpectedDelay / 5
+
+	height := int64(10)
+	addedDiff := -1 * (maxDeflection - maxDeflection/2)
+
+	header := types.Header{
+		Height: height,
+		Time:   genesisTime.Add(9 * time.Second).Add(addedDiff),
+	}
+
+	currentBlock := &types.Block{
+		Header: header,
+	}
+
+	blockStore.On("Load").Return(mockState, nil)
+	blockStore.On("LoadConsensusParams", (height-1)).Return(consensusParams, nil)
+
+	delay := state.CalculateDelay(blockStore, currentBlock)
+
+	// as addedDiff is less than the maxDeflection we expect the idealExpectedDelay - addedDiff
+	expectedDelay := idealExpectedDelay - addedDiff
+
+	assert.Equal(t, expectedDelay, delay)
+}
